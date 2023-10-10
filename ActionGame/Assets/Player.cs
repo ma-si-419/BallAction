5a09306099fa569877c1737cb697d27aaa205198
ma-si- <Kouma9140@gmail.com>
2023-10-10 09:43:52 +0900

    up

--- ActionGame/Assets/Player.cs
+++ ActionGame/Assets/Player.cs
@@ -6,26 +6,40 @@ public class Player : MonoBehaviour
 {
     TestInputActions testInputActions;
 
-    float Speed = 0.01f;
-    float StickX;
-    float StickZ;
-    float MoveX;
-    float MoveZ;
+    float _speed = 10000f;
+    float _stickX;
+    float _stickZ;
+    float _moveDir;
+    float _moveSpeed;
+    float _jumpPower;
+    Vector3 _move;
+    GameObject _player;
+    Rigidbody _rb;
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        // ƒWƒƒƒ“ƒv‚·‚é—Í‚ð—^‚¦‚é
+        _rb.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
+    }
     void Start()
     {
+        _rb = GetComponent<Rigidbody>();
         testInputActions = new TestInputActions();
         testInputActions.Enable();
         testInputActions.Player.Fire.performed += context => Debug.Log("Fire");
+
     }
 
     void Update()
     {
 
         var direction = testInputActions.Player.Move.ReadValue<Vector2>();
-        StickX= direction.x;
-        StickZ = direction.y;
-        MoveX += StickX * Speed;
-        MoveZ += StickZ * Speed;
-        this.transform.Translate(MoveX,0,MoveZ);
+        _stickX= direction.x;
+        _stickZ = direction.y;
+        _move = new Vector3(_speed * _stickX,0, _speed * _stickZ);
+        _rb.AddForce(_move);
+        
+        
+        
+        
     }
 }
